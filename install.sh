7ab4cc514f1d0b3798496b22434f1917c8a32435
Jonathan Clem <j@jclem.me>
2022-02-07 17:12:33 -0500

    Always pull Codespaces dotfiles from git

--- install.sh
+++ install.sh
@@ -17,7 +17,5 @@ else
   chezmoi=chezmoi
 fi
 
-# POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
-script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
-# exec: replace current process with chezmoi init
-exec "$chezmoi" init --apply "--source=$script_dir"
+# Apply from dotfiles repo, ignore our local dotfiles.
+exec "$chezmoi" init jclem --apply --verbose
