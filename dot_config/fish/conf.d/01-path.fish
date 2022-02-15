8f14a0f197049fff60e0599c7d6bf4c965ada4e9
Jonathan Clem <j@jclem.me>
2022-02-15 10:23:50 -0500

    Add $HOME/.local/bin to PATH in Linux

--- dot_config/fish/conf.d/01-path.fish
+++ dot_config/fish/conf.d/01-path.fish
@@ -2,6 +2,10 @@ if test (uname) = Darwin
     set -x PATH $PATH /opt/homebrew/bin "/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
 end
 
+if test (uname) = Linux
+    set -x PATH $PATH "$HOME/.local/bin"
+end
+
 set -x PATH $PATH "$HOME/bin"
 
 if type -q go
