78e91fd2a67c9eacbd8f2e5a6d03c96451f78e34
Jonathan Clem <j@jclem.me>
2022-06-23 08:45:19 -0400

    Fix r completion

--- dot_config/fish/completions/r.fish
+++ dot_config/fish/completions/r.fish
@@ -1,6 +1,5 @@
-
 complete --command r \
-    --condition '__fish_is_token_n 2' \
+    --condition '__fish_is_nth_token 1' \
     --arguments '(__r_user_completion)' \
     --no-files
 
@@ -10,7 +9,7 @@ function __r_user_completion
 end
 
 complete --command r \
-    --condition '__fish_is_token_n 3' \
+    --condition '__fish_is_nth_token 2' \
     --arguments '(__r_repo_completion)' \
     --no-files
 
