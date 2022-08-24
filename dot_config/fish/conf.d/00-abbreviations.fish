fdb731d2660a51c1756650d000a40853f88dfab2
Jonathan Clem <j@jclem.me>
2022-08-24 16:03:54 -0400

    Remove code-insiders abbreviation

--- dot_config/fish/conf.d/00-abbreviations.fish
+++ dot_config/fish/conf.d/00-abbreviations.fish
@@ -2,7 +2,6 @@
 
 if status --is-interactive
     abbr --add --global bs brew services
-    abbr --add --global code code-insiders
     abbr --add --global cz chezmoi
     abbr --add --global dc docker-compose
     abbr --add --global dk docker
