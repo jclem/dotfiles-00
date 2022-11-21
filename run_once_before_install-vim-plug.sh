fbf900278176b8a9e6a2891fdf0dd1a070a28e4a
Jonathan Clem <jclem@github.com>
2022-11-21 13:32:58 -0500

    Install fewer things in Codespaces

--- run_once_before_install-vim-plug.sh
+++ run_once_before_install-vim-plug.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env -S bash -x
 
+{{ if (eq .chezmoi.os "darwin") -}}
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+{{ end -}}
