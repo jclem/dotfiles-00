1e87d2c009cb38f3a6a474f869cbe01c87d1dce8
Jonathan Clem <j@jclem.me>
2023-03-14 16:24:31 -0400

    Add -a flag to reload function

--- dot_config/fish/functions/reload.fish
+++ dot_config/fish/functions/reload.fish
@@ -1,4 +1,18 @@
 function reload
-    chezmoi update
+    argparse --name=reload a/apply h/help -- $argv
+
+    if set -q _flag_h
+        echo "Usage: reload [-a]"
+        echo "Reloads the shell with the latest dotfiles."
+        echo "  -a, --apply  Apply the dotfiles instead of updating them."
+        return
+    end
+
+    if set -q _flag_a
+        chezmoi apply
+    else
+        chezmoi update
+    end
+
     exec fish
 end
