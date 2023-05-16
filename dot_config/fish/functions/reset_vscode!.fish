0f5374bfb1c3bc04f858adbf3adf77e1f681de49
Jonathan Clem <j@jclem.me>
2023-05-16 09:40:33 -0400

    Reset Insiders with reset_vscode!

--- dot_config/fish/functions/reset_vscode!.fish
+++ dot_config/fish/functions/reset_vscode!.fish
@@ -1,7 +1,13 @@
 function reset_vscode!
-    echo '[]' > "$HOME/Library/Application Support/Code/User/keybindings.json"
-    echo '{}' > "$HOME/Library/Application Support/Code/User/settings.json"
+    echo '[]' >"$HOME/Library/Application Support/Code/User/keybindings.json"
+    echo '{}' >"$HOME/Library/Application Support/Code/User/settings.json"
     for ext in (code --list-extensions)
         code --uninstall-extension $ext --force
+    end
+
+    echo '[]' >"$HOME/Library/Application Support/Code - Insiders/User/keybindings.json"
+    echo '{}' >"$HOME/Library/Application Support/Code - Insiders/User/settings.json"
+    for ext in (code-insiders --list-extensions)
+        code-insiders --uninstall-extension $ext --force
     end
 end
