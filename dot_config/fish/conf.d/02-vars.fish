93c18146cc8986bab25dde4d464d375c660931e4
Jonathan Clem <j@jclem.me>
2023-08-22 11:37:46 -0400

    Set EDITOR to code-insiders

--- dot_config/fish/conf.d/02-vars.fish
+++ dot_config/fish/conf.d/02-vars.fish
@@ -1,5 +1,5 @@
 set -x CODE "$HOME/src/github.com"
-set -x EDITOR "code --wait"
+set -x EDITOR "code-insiders --wait"
 set -x ERL_AFLAGS "-kernel shell_history enabled -kernel shell_history_file_bytes 2097152"
 set -x GPG_TTY (tty)
 set -x XDG_CONFIG_HOME "$HOME/.config"
