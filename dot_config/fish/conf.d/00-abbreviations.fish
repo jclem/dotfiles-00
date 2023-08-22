20ab6c34efe22145b6da748de40ec82b6bbdb7b9
Jonathan Clem <j@jclem.me>
2023-08-22 11:37:36 -0400

    Add abbreviations

--- dot_config/fish/conf.d/00-abbreviations.fish
+++ dot_config/fish/conf.d/00-abbreviations.fish
@@ -1,12 +1,14 @@
 # See: https://fishshell.com/docs/current/commands.html#abbr
 
 if status --is-interactive
+    abbr --add --global b bots
     abbr --add --global bs brew services
     abbr --add --global co cobble
     abbr --add --global code code-insiders
     abbr --add --global cz chezmoi
     abbr --add --global dc docker-compose
     abbr --add --global dk docker
+    abbr --add --global fly flyctl
     abbr --add --global g git
     abbr --add --global k konk
     abbr --add --global ls exa
